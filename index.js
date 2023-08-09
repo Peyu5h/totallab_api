6fe4b4b4278105d002111ee5c241a95f02cf767e
Mihir <118926177+MihirGit1@users.noreply.github.com>
2023-08-09 22:56:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from the API!' });
+  res.send('Hello from the API!');
 });
 
 // Define a simple route
