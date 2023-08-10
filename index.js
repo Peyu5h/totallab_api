c3c7f281c4d2a41c3009c73f2f37d351b944de25
MihirGit1 <mihirgrand@gmail.com>
2023-08-10 18:58:53 +0530

    Added a status subroute to a new route called api

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("Totallab Validation API homepage!");
 });
 
+app.get("/api/status", (req, res) => {
+  res.send("Status: Online");
+});
+
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from the API!" });
 });
