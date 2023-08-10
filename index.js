79232b25a3b42c87d154466cfb66c5000cb7a8c7
MihirGit1 <mihirgrand@gmail.com>
2023-08-10 18:47:42 +0530

    First commit from Fedora

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Home!');
+app.get("/", (req, res) => {
+  res.send("Home!");
 });
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from the API!' });
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from the API!" });
 });
 
 app.use((req, res) => {
-  res.status(404).json({ error: 'Route not found' });
+  res.status(404).json({ error: "Route not found" });
 });
 
 const port = process.env.PORT || 3000;
