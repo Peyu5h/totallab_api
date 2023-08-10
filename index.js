c22d5ba9054964b2928dbfbb181b7e9c22db33e8
MihirGit1 <mihirgrand@gmail.com>
2023-08-10 18:51:39 +0530

    First commit from Fedora

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Home!");
+  res.send("Totallab Validation API!");
 });
 
 app.get("/api/hello", (req, res) => {
