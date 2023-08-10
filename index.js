9e2a442eb88af57db9ed548ebcc2e3e4b1fb1a86
MihirGit1 <mihirgrand@gmail.com>
2023-08-10 20:20:30 +0530

    Gitignore

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+require("dotenv").config();
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoString = process.env.MONGO_URI;
 const app = express();
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Totallab Validation API homepage!");
