8ae11f444837b78b659104b913ef52e7d11ac789
Mihir <118926177+MihirGit1@users.noreply.github.com>
2023-08-10 17:54:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ app.get('/', (req, res) => {
   res.send('Hello from the API!');
 });
 
-// Define a simple route
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from the API!' });
 });
 
-// Handle 404 - Route not found
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
 
-// Start the server
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
